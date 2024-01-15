0af4e9df6fc451a542af053f4c93cf9895d6f9ed
Ruchhit <ruchitgule2219@gmail.com>
2024-01-15 18:14:17 +0530

    user model nd mongo conncetion

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,16 @@
-var express = require('express');
-var router = express.Router();
-
-/* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
-});
-
-module.exports = router;
+ const mongoose = require('mongoose')
+ require("dotenv").config();
+ mongoose.connect(process.env.MONGO_URI)
+ .then(()=>{
+  console.log("connected to mongooo")
+ })
+  .catch(()=> {console.log("cant conect to mongoo")})
+ const userSchema = new mongoose.Schema({
+   username : {type:String},
+   name : {type:String},
+   email : {type:String},
+   password : {type:String},
+   profileImage : {type:String},
+   boards : { type : Array , default : []}
+ });
+ module.exports = mongoose.model("User",userSchema)
