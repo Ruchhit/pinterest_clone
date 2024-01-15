e634bd5e92903d1654a04109ddfb678439f4beeb
Ruchhit <ruchitgule2219@gmail.com>
2024-01-15 19:43:13 +0530

    update:user can successfully register,with db entry

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,22 @@
 var express = require('express');
+const passport = require('passport');
 var router = express.Router();
+const userModel =require ('../routes/users')
 
+const localStrategy = require("passport-local");
+passport.use(new localStrategy(userModel.authenticate()))
 /* GET home page. */
+
+const isLoggedIn = (req,res,next)=>{
+  if(req.isAuthenticated())
+  {
+    return next()
+  }
+  res.redirect("/");
+}
+router.get("/profile",isLoggedIn,(req,res)=>{
+  res.render("profile")
+})
 router.get('/', function(req, res, next) {
   res.render('index');
 });
@@ -9,5 +24,37 @@ router.get('/', function(req, res, next) {
 router.get("/register",(req,res)=>{
   res.render('register');
 })
+ 
+
+router.post("/register",(req,res)=>{
+  var userData = new userModel({
+    username : req.body.username,
+    name : req.body.name,
+    email : req.body.email,
+  });
+  userModel.register(userData,req.body.password)
+  .then(()=>passport.authenticate("local")(req,res,()=>{res.redirect("/profile")}))
+
+})
+
+router.post("/login", passport.authenticate("local", {
+  successRedirect: "/profile",
+  failureRedirect: "/"
+}));
+
+
+router.get("/logout",(req,res,next)=>{
+  req.logout((err)=>{
+    if(err) return nrxt(err);
+    res.redirect("/")
+  })
+})
+
+ 
+
+
+
+
 
 module.exports = router;
+
--- routes/users.js
+++ routes/users.js
@@ -1,11 +1,13 @@
  const mongoose = require('mongoose')
  require("dotenv").config();
+ const plm = require('passport-local-mongoose')
  mongoose.connect(process.env.MONGO_URI)
  .then(()=>{
   console.log("connected to mongooo")
  })
   .catch(()=> {console.log("cant conect to mongoo")})
- const userSchema = new mongoose.Schema({
+ 
+  const userModel = new mongoose.Schema({
    username : {type:String},
    name : {type:String},
    email : {type:String},
@@ -13,4 +15,6 @@
    profileImage : {type:String},
    boards : { type : Array , default : []}
  });
- module.exports = mongoose.model("User",userSchema)
+
+ userModel.plugin(plm)
+ module.exports = mongoose.model("User",userModel)
